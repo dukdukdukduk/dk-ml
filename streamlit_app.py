1c1c328e9c0727e7b2624dfb2a8fe2c9e1d2259d
dukdukdukduk <314dlgn@gmail.com>
2024-09-05 15:30:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,15 @@ st.title('🗿 App Testing')
 
 st.write('1 2 1 2 mic check, mic check!')
 
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('aimag_data.csv')
+  df
+
+  st.write('**X**')
+  X = df.drop('Centre', axis = 1)
+  X
+
+  st.write('**y**')
+  y = df.Centre
+  y
